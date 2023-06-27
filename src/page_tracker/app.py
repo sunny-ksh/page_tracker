cca9f057c99e228c49379a5e20ee5e7465939603
sunny-ksh <rlatjsgnl123@kyonggi.ac.kr>
2023-06-28 07:14:59 +0900

    test client

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,10 +1,27 @@
+from functools import cache
 from flask import Flask
-from redis import Redis
+from redis import Redis, RedisError
 
 app = Flask(__name__)
-redis = Redis()
+''' Dependency injection
+	redis = Redis()
+'''
 
 @app.get("/")
 def index():
-    page_views = redis.incr("page_views")
-    return f"This page has been seen {page_views} times."
+# Dependency injection
+# page_views = redis.incr("page_views")
+
+    try:
+        page_views = redis().incr("page_views")
+    except RedisError:
+        app.logger.exception("Redis error")
+        return "Sorry, something went wrong \N{pensive face}", 500
+    else:
+        return f"This page has been seen {page_views} times."
+
+	return f"This page has been seen {page_views} times."
+
+@cache
+def redis():
+	return Redis()
